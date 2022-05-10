634706584c5d37b5e0f799c470025a7d9d865e83
Knight <liuxiang147@163.com>
2022-05-10 15:35:00 +0800

    Algo-Go

---
+++ BinarySearch/binarySearch_less.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 在 有序数组 中 查找 第一个 小于 target 的 元素 的 索引位置
+	arr := []int{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(binarySearch_less(arr, 7))
+}
+
+// Time: O(logn)
+// Space: O(1)
+func binarySearch_less(arr []int, target int) int {
+	left, right := -1, len(arr)
+	for left+1 != right {
+		mid := left + (right-left)>>1
+		if arr[mid] < target {
+			left = mid
+		} else {
+			right = mid
+		}
+	}
+	return left
+}
